a558ad3e05ae6c693e41d0f4d8d41556bd9380e7
edgar <e.gm27@outlook.com>
2025-12-29 14:27:41 -0700

    cambios en MVC

--- public/index.php
+++ public/index.php
@@ -10,7 +10,10 @@ $router = new Router();
 
 $router->get('/admin', [PropiedadControladora::class, 'index']);
 $router->get('/propiedades/crear', [PropiedadControladora::class, 'crear']);
+$router->post('/propiedades/crear', [PropiedadControladora::class, 'crear']);
 $router->get('/propiedades/actualizar', [PropiedadControladora::class, 'actualizar']);
+$router->post('/propiedades/actualizar', [PropiedadControladora::class, 'actualizar']);
+$router->post('/propiedades/eliminar', [PropiedadControladora::class, 'eliminar']);
 
 
 $router->ComprobarRutas();
