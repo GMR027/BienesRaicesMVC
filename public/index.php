71ba52beea2d5ab18b14132fb03c966d08ef6d7b
edgar <e.gm27@outlook.com>
2026-01-01 14:55:15 -0700

    Mostrar vendedores y funciones crud

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\PropiedadControladora;
+use Controllers\VendedorController;
 
 $router = new Router();
 
@@ -14,7 +15,10 @@ $router->post('/propiedades/crear', [PropiedadControladora::class, 'crear']);
 $router->get('/propiedades/actualizar', [PropiedadControladora::class, 'actualizar']);
 $router->post('/propiedades/actualizar', [PropiedadControladora::class, 'actualizar']);
 $router->post('/propiedades/eliminar', [PropiedadControladora::class, 'eliminar']);
-
+$router->get('/vendedores/crear', [VendedorController::class, 'crear']);
+$router->post('/vendedores/crear', [VendedorController::class, 'crear']);
+$router->get('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
+$router->post('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
 
 $router->ComprobarRutas();
 
